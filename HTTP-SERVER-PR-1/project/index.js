64f6f88217aa50ee7ad0ca618125c0a24a3f1ef0
dhruvghinaiya12 <dhruvghinaiya12@gmail.com>
2024-10-22 22:40:28 +0530

    node

--- HTTP-SERVER-PR-1/project/index.js
+++ HTTP-SERVER-PR-1/project/index.js
@@ -2,26 +2,22 @@ const http = require("http");
 const fs = require("fs");
 
 const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/html" });
   if (req.url == "/") {
     fs.readFile("index.html", (err, data) => {
       if (err) {
         console.log(err);
       } else {
-        res.writeHead(200, { "Content-Type": "text/html" });
         res.end(data);
       }
     });
   } else if (req.url == "/login") {
-    res.writeHead(200, { "Content-Type": "text/html" });
     res.end(`login successfully`);
   } else if (req.url == "/signup") {
-    res.writeHead(200, { "Content-Type": "text/html" });
     res.end(`sign up successfully`);
   } else if (req.url == "/contact") {
-    res.writeHead(200, { "Content-Type": "text/html" });
     res.end(`Welcome to contact page`);
   } else if (req.url == "/service") {
-    res.writeHead(200, { "Content-Type": "text/html" });
     res.end(`Welcome to service page`);
   }
 });
