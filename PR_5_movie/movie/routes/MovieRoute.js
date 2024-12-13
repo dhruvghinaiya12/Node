58d4a6492591c43af600c8acefa958042b3333a3
dhruvghinaiya12 <dhruvghinaiya12@gmail.com>
2024-12-13 23:52:53 +0530

    node

---
+++ PR_5_movie/movie/routes/MovieRoute.js
@@ -0,0 +1,16 @@
+const {Router} = require("express");
+const { createMovie, getAllMovies, getMovieById, updateMovie, deleteMovie, addRating, addComment, getFilteredMovies } = require("../controller/MovieController");
+
+
+const MovieRoute =Router();
+
+MovieRoute.post("/create",  createMovie);
+MovieRoute.get("/", getAllMovies);
+MovieRoute.get("/:id", getMovieById);
+MovieRoute.patch("/update/:id", updateMovie);
+MovieRoute.delete("/delete/:id", deleteMovie);
+MovieRoute.patch("/rating/:id", addRating);
+MovieRoute.patch("/comment/:id", addComment);
+MovieRoute.get("/filter", getFilteredMovies);
+
+module.exports = MovieRoute;
